8bfa4631d1b30701276c97f0b5f6db424ec29575
geodoer <geodoer@outlook.com>
2022-04-21 20:21:08 +0800

    libigl-samples project
    Signed-off-by: geodoer <geodoer@outlook.com>

---
+++ libigl-samples/readme.md
@@ -0,0 +1,2 @@
+此工程模板来自：https://github.com/libigl/libigl-example-project
+
--- libigl-samples/simple/conanbuild.bat
+++
@@ -1,6 +0,0 @@
-rd /s /q build
-mkdir build
-cd build
-conan install .. -s compiler.version=16
-cmake .. -G "Visual Studio 16"
-pause
