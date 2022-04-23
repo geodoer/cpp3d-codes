89ee859f231a8d88393853ca0d4e1538562a2784
geodoer <geodoer@outlook.com>
2022-04-23 15:44:49 +0800

    libigl+cgal+表面细分
    Signed-off-by: geodoer <geodoer@outlook.com>

--- libigl-samples/readme.md
+++ libigl-samples/readme.md
@@ -6,3 +6,9 @@
 如果你本地下载了libigl
 1. 打开`build.bat`，修改`FETCHCONTENT_SOURCE_DIR_LIBIGL`的值
 2. 运行build.bat即可
+
+
+## 问题
+### 运行exe报错：找不到libgmp-10.dll
+1. 在`./build/_deps`目录下搜索`libgmp-10.dll`
+2. 拷贝到exe所在目录
--- libigl-samples/src/simple/cgal/Subdivision_method.cpp
+++ libigl-samples/src/simple/cgal/Subdivision_method.cpp
@@ -1,16 +1,21 @@
-﻿#include <CGAL/Simple_cartesian.h>
-#include <CGAL/Surface_mesh.h>
-#include <CGAL/boost/graph/graph_traits_Surface_mesh.h>
-#include <CGAL/subdivision_method_3.h>
+﻿#include <iostream>
 #include <CGAL/Timer.h>
-#include <boost/lexical_cast.hpp>
-#include <iostream>
-#include <fstream>
 
+//CGAL Polyhedron_3
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/Polyhedron_items_with_id_3.h>
+
+//CGAL 细分算法
+#include <CGAL/subdivision_method_3.h>
+
+//igl工具
+#include <igl/copyleft/cgal/polyhedron_to_mesh.h>
+#include <igl/copyleft/cgal/mesh_to_polyhedron.h>
+
+//igl IO
 #include <igl/readOFF.h>
-
-typedef CGAL::Simple_cartesian<double>          Kernel;
-typedef CGAL::Surface_mesh<Kernel::Point_3>     PolygonMesh;
+#include <igl/writeOFF.h>
 
 using namespace std;
 using namespace CGAL;
@@ -18,31 +23,40 @@ namespace params = CGAL::parameters;
 
 int main(int argc, char **argv)
 {
-	const std::string in_file = R"(C:\Users\2107\Desktop\bunny.off)";       //输入Mesh(.off)
+	const std::string in_file = R"(C:\Users\2107\Desktop\bunny.off)";					//输入Mesh(.off)
 	int d = 1;																			//细分深度the depth of the subdivision
-	const char* out_file = R"(C:\Users\2107\Desktop\Sqrt3_subdivision_result.off)";   //输出Mesh(.off)
+	const char* out_file = R"(C:\Users\2107\Desktop\Sqrt3_subdivision_result.off)";		//输出Mesh(.off)
 
-	////off -> igl
+	///off -> igl
 	Eigen::MatrixXd V;
 	Eigen::MatrixXi F;
 	igl::readOFF(in_file, V, F);
 
-	////igl -> cgal
-	
+	///igl -> cgal
+	CGAL::Polyhedron_3<
+		CGAL::Simple_cartesian<double>,
+		CGAL::Polyhedron_items_with_id_3,
+		CGAL::HalfedgeDS_default,
+		std::allocator<int> >  mesh;
 
-	////使用CGAL的细分算法
-	PolygonMesh pmesh;
+	if (!igl::copyleft::cgal::mesh_to_polyhedron(V, F, mesh))
+	{
+		std::cout << "mesh_to_polyhedron fatal";
+		return 0;
+	}
+
+	///使用CGAL的细分算法
 	Timer t;
 	t.start();
-	Subdivision_method_3::Sqrt3_subdivision(pmesh, params::number_of_iterations(d));
+	Subdivision_method_3::Sqrt3_subdivision(mesh, params::number_of_iterations(d));
 	std::cerr << "Done (" << t.time() << " s)" << std::endl;
 
-	////cgal -> igl
+	///cgal -> igl
+	Eigen::MatrixXd V2;
+	Eigen::MatrixXi F2;
+	igl::copyleft::cgal::polyhedron_to_mesh(mesh, V2, F2);
 
-	////igl -> off
-
-	
-
-	
+	///igl -> off
+	igl::writeOFF(out_file, V2, F2);
 	return 0;
 }
